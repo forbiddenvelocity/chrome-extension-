c935398ff994ecb42033da2deeee1ec9b7da4cc2
Mubashir <mubashir1728@gmail.com>
2023-08-27 14:58:46 +0530

    changes

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-let myLeads = ["www.4chan.com", "www.codeforces.com", "www.reddit.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
 
-
-for(let i=0; i<myLeads.length; i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+function renderLeads(){
+    let listItems = " "
+    for(let i=0; i<myLeads.length; i++){
+        // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+        // create element
+        // set text content
+        // append to ul
+        //const li = document.createElement("li")
+        //li.textContent = myLeads[i]
+        //ulEl.append(li)
+        listItems += "<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</li>"
+    }
+    
+    ulEl.innerHTML = listItems
 }
+
+
+
+
 
