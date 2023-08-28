016744333d18671d066a639690a1be42a7327d98
Mubashir <mubashir1728@gmail.com>
2023-08-28 22:11:21 +0530

    commit

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+localStorage.clear
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
 })
 
